ba126c82273b7b4f430821d414f04eab33ef9b84
masafiedin <masafiedin24@gmail.com>
2023-02-26 23:14:03 +0200

    Implemetned String games

--- games.js
+++ games.js
@@ -42,3 +42,67 @@ function mergeSort() {
   let sortedArray = mergeSort(arr);
   document.getElementById("mergeSortOutput").innerHTML = String(sortedArray);
 }
+
+function palindrome() {
+  function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    str = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+
+    // Compare the original string with its reverse
+    return str === str.split("").reverse().join("");
+  }
+
+  // Prompt the user for a string input
+  const input = prompt("Enter a word or phrase:");
+  document.getElementById("palindromeOutput").innerHTML = input;
+  // Check if the input is a palindrome
+  if (isPalindrome(input)) {
+    document.getElementById("palindromeOutput").innerHTML =
+      " The provided string is a palindorme.".concat(input);
+  } else {
+    document.getElementById("palindromeOutput").innerHTML =
+      " The provided string is not a palindorme: ".concat(input);
+  }
+}
+
+function reverseString() {
+    let numberArray = [];
+    let indexArray = [];
+  // this function will extract all numbers from a string,
+  // and the indices at which these numbers were presented.
+  function extractNumbersAndIndices(str) {
+    for (let i = 0; i < str.length; i++) {
+      const char = str[i];
+      if (!isNaN(char) && char !== " ") {
+        numberArray.push(parseInt(char));
+        indexArray.push(i);
+      }
+    }
+  }
+
+  //swapping numbers by order
+  //example: string: He11oPe0pl3 s(11) . numberArray = [1,1,0,3]. indexArray = [2,3,7,10]
+  let input = prompt("Enter a phrase that contains numbers").split("");
+    //checking if input has numbers
+    if (/\d/.test(input)) {
+      extractNumbersAndIndices(input);
+
+      let nums = numberArray.length;
+      for (var i = 0; i < nums; i++) {
+        var indexOfi = indexArray[i];
+        input[indexOfi] = numberArray[(nums - 1) - i];
+        console.log("lho",input[indexOfi]);
+        console.log("rho",numberArray[(nums - 1) - i]);
+
+      }
+      document.getElementById("reverseStrinOutput").innerHTML =
+        "This is the input, with numbers reversed :D ".concat(input.join(""));
+    } else {
+      var inputReverse = input.split("").reverse().join("");
+      document.getElementById("reverseStrinOutput").innerHTML =
+        "You have entered a string with no numbers, might as well be reversed: ".concat(
+          inputReverse
+        );
+    }
+
+}
